19adedf96c938126fa4ecc2fb7018fd20ea8cdc6
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-02 16:47:52 -0700

    added ajax and data

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,22 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 
 $(document).on("ready", function() {
 
-  // CODE IN HERE!
+  function getQuakes(){
+    $.ajax({
+      method: 'GET',
+      url: weekly_quakes_endpoint,
+      dataType: 'JSON',
+      success: onSuccess
+    });
+  }
+  function onSuccess(json){
+    var source = $('#earthquake-li-templete').html();
+    var template = Handlebars.compile(source);
+    var quakesHtml = template({earthquakes: json.features})
+    $('#earthquake-list').append(quakesHtml);
 
+
+
+  }
+getQuakes();
 });
