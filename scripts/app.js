cb6f38b1dd5cabde58abc1c0677d8315bf5cfe95
DANGERSTEPHEN <DANGERSTEVE1@GMAILCOM>
2016-11-02 18:06:20 -0700

    added map and function to work with pins

--- scripts/app.js
+++ scripts/app.js
@@ -12,13 +12,33 @@ $(document).on("ready", function() {
     });
   }
   function onSuccess(json){
+    var features= json.features
     var source = $('#earthquake-li-templete').html();
     var template = Handlebars.compile(source);
-    var quakesHtml = template({earthquakes: json.features})
+    var quakesHtml = template({earthquakes: features})
     $('#earthquake-list').append(quakesHtml);
-
-
-
   }
+
+  function initMap() {
+         var uluru = {lat: -25.363, lng: 131.044};
+         var map = new google.maps.Map(document.getElementById('map'), {
+           zoom: 4,
+           center: uluru
+         });
+
+         function getPins(features) {
+                 for (var i = 0; i < features.length; i++) {
+                   var coords = features[i].geometry.coordinates;
+                   var latLng = new google.maps.LatLng(coords[1],coords[0]);
+                   var marker = new google.maps.Marker({
+                     position: latLng,
+                     map: map
+                   });
+                 }
+               }
+       }
+
 getQuakes();
+initMap();
+getPins(features);
 });
